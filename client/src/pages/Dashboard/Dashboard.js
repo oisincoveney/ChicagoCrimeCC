458c76bb4b7c0a6aca82635036f8b5a69efdee47
Oisin Coveney <oisinthec@gmail.com>
2020-12-11 11:38:10 -0800

    Added tailwind for easier css classes

--- client/src/pages/Dashboard/Dashboard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Dashboard = () => {
-
-    return "This is dashboard page";
-}
-
-export default Dashboard;
---
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './Dashboard.styles';
+
+const Dashboard = (props) => (
+  <div className="DashboardWrapper">
+    Test content
+  </div>
+);
+
+Dashboard.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Dashboard.defaultProps = {
+  // bla: 'test',
+};
+
+export default Dashboard;
