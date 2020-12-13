3b1eed62132d5e250f44d82e2495b72e26eaba8c
Oisin Coveney <oisinthec@gmail.com>
2020-12-13 10:45:41 -0800

    Added a package

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 //import { Test } from './Dashboard.styles';
+import * as d3 from 'd3';
 
-const Dashboard = (props) => (
-  <div className="DashboardWrapper">
-    Test content
-  </div>
-);
+const Dashboard = (props) => {
+
+  let drawChart = () => {
+    const width = 800;
+    const height = 450;
+    const el = new Element('div');
+    const svg = d3.select(el)
+      .append('svg')
+      .attr('id', 'chart')
+      .attr('width', width)
+      .attr('height', height);
+
+    return el.toReact();
+  }
+
+  return (
+    <div className="DashboardWrapper">
+      {drawChart()}
+    </div>
+  )
+};
 
 Dashboard.propTypes = {
   // bla: PropTypes.string,
