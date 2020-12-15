0eeda8fe4c89e6f507abbab3afeb7697b95d1e1c
Oisin Coveney <oisinthec@gmail.com>
2020-12-15 13:31:28 -0800

    testing firebase connectivity in dashboard

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import BarChart from './BarChart/index'
-import { useFirestoreDocData, useDatabase, SuspenseWithPerf } from 'reactfire';
+import { useDatabase, SuspenseWithPerf } from 'reactfire';
 import 'firebase/database'
 
 
@@ -9,18 +9,35 @@ const Dashboard = (props) => {
   const container = useRef(null)
   const testRef = useDatabase()
 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    let tmp = []
+    testRef.ref('Crimes/').once('value').then(snapshot => {
+      tmp.push(snapshot.id)
+    }).then(() => {
+      setData(tmp)
+    })
+  })
+
+  
   // const {status, data} = useFirestoreDocData(testRef)
   
+
   // if (status === 'loading') {
     // return <h1>Hello!!</h1>
   // }
+
+
+
+  
 
   return (
     <SuspenseWithPerf fallback={'loading burrito status...'} traceId={'load-burrito-status'}>
       <div id="vis-container" ref={container}></div>
       <BarChart></BarChart>
       {JSON.stringify(Object.keys(testRef))}
-      {/* {JSON.stringify(data)} */}
+      {JSON.stringify(data)}
     </SuspenseWithPerf>
   )
 };
